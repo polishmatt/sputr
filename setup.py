5e3764983f8190665758aac565b4c2bec5706332
Matt Wisniewski <hello@mattw.us>
2017-01-18 22:20:51 -0500

    get setuptools working

--- setup.py
+++ setup.py
@@ -1,14 +1,17 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name='sputr',
     version='0.1',
+    packages=[
+        'sputr'
+    ],
     install_requires=[
         'click==6.6'
     ],
     entry_points={
         'console_scripts': [
-            'sputr = sputr:cli'
+            'sputr = sputr.cli:cli'
         ],
     },
 )
