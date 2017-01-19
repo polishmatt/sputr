55e3e976e0a15f104bead3636ba1f52d9798db01
Matt Wisniewski <hello@mattw.us>
2017-01-19 00:42:01 -0500

    allow .py or .pyc

--- tests/test_import.py
+++ tests/test_import.py
@@ -4,11 +4,12 @@ import os
 
 class ImportTest(unittest.TestCase):
     def test_import(self):
-        expected = os.path.join(os.path.join(os.path.realpath(os.path.join(os.path.dirname(__file__), '..')), 'sputr'), 'sputr.pyc')
+        expected = os.path.join(os.path.join(os.path.realpath(os.path.join(os.path.dirname(__file__), '..')), 'sputr'), 'sputr.py')
         found = sputr.__file__
-        self.assertEqual(found, expected, """
-            Version of sputr imported is not the version we want to test
-            Found: '%s'
-            Expected: '%s'
-        """ % (found, expected))
+        if found not in [expected, expected + 'c']:
+            self.fail("""
+                Version of sputr imported is not the version we want to test
+                Found: '%s'
+                Expected: '%s'
+            """ % (found, expected))
 
