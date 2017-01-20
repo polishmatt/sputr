42b393185153b6bd875d5055811c65616ca9d16b
Matt Wisniewski <hello@mattw.us>
2017-01-19 20:28:56 -0500

    make sure cwd is at the front of path so we can't import the wrong packages

--- sputr/cli.py
+++ sputr/cli.py
@@ -2,11 +2,16 @@ import click
 import unittest
 import sputr
 import config
+import sys
+import os
 
 @click.command()
 @click.version_option(version=config.version)
 @click.argument('pattern', default='')
 def cli(pattern):
+    if sys.path[0] != os.getcwd():
+        sys.path.insert(0, os.getcwd())
+
     suite = sputr.discover(pattern=pattern)
     unittest.TextTestRunner().run(suite)
 
