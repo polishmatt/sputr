ce87db5361c2a4fa3a3df75e3513dc371850e009
Matt Wisniewski <hello@mattw.us>
2017-01-19 21:39:53 -0500

    expose start_dir

--- sputr/cli.py
+++ sputr/cli.py
@@ -8,11 +8,12 @@ import os
 @click.command()
 @click.version_option(version=config.version)
 @click.argument('pattern', default='')
-def cli(pattern):
+@click.option('--start_dir', default='.', show_default=True)
+def cli(pattern, start_dir):
     if sys.path[0] != os.getcwd():
         sys.path.insert(0, os.getcwd())
 
-    suite = sputr.discover(pattern=pattern)
+    suite = sputr.discover(pattern=pattern, start_dir=start_dir)
     unittest.TextTestRunner().run(suite)
 
 if __name__ == '__main__':
