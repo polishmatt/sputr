3f813c254a5eab21c64144d7c053f658b9fbd7b6
Matt Wisniewski <hello@mattw.us>
2017-02-09 00:47:27 -0500

    add examples to help option and support -h

--- sputr/cli.py
+++ sputr/cli.py
@@ -6,7 +6,20 @@ import os
 import sputr
 from .config import version
 
-@click.command()
+@click.command(
+    help='Simple Python Unit Test Runner',
+    epilog="""\b\bExamples:\n
+sputr                      -run all tests in the current directory\n
+sputr dirname              -run all tests in the specified directory\n
+sputr filename.py          -run all tests in files with this name\n
+sputr dirname/filename.py  -run all tests in a specific file\n
+sputr test_name            -run all tests that match a pattern\n
+sputr test_name_*          -run all tests that match a pattern
+    """,
+    context_settings={
+        'help_option_names': ['-h','--help'],
+    }
+)
 @click.argument('pattern', default='')
 @click.version_option(version=version)
 @click.option('--start_dir', '-s', default='.', show_default=True, help='Directory to start discovery')
@@ -17,6 +30,7 @@ from .config import version
 @click.option('--catch', '-c', is_flag=True, help='Display test results after keyboard interrupt (control-c)')
 @click.option('--top_level_dir', '-t', help='Top level directory of the project')
 def cli(pattern, start_dir, verbose, quiet, failfast, buffer, catch, top_level_dir):
+
     if sys.path[0] != os.getcwd():
         sys.path.insert(0, os.getcwd())
 
